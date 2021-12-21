0916bedb6bf258082c079c4b26d40dc3d7ae29e0
wfrsnk <57827569+wfrsnk@users.noreply.github.com>
2021-12-21 18:09:43 +0300

    add task 3b

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 import  express  from 'express';
 import ejs from 'ejs';
 import fetch from 'node-fetch';
+import converter from 'xml-js';
 
+const PORT = process.env.PORT || 80;
 const app = express();
-
-// fetch('http://www.cbr.ru/scripts/XML_daily.asp')
-//         .then(response => response.text())
-//         .then(data => console.log(data));
 
 app
     .set("view engine", "ejs")
     .set("views", "views")
 
-
+app.get('/api/data', (req, res) => {
+    fetch('http://www.cbr.ru/scripts/XML_daily.asp')
+        .then(response => response.text())
+        .then(data => res.send(JSON.parse(converter.xml2json(data, {compact: true, spaces: 4}))));
+    })
 app.get('/', (req, res) => {
     res.render('./index');
 }) 
 
-
-app.listen(80, () => {
+app.listen(PORT, () => {
     console.log('Server has been started...');
 })
